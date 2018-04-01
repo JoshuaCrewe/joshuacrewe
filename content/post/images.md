d10ce6d52bee71cf637fb00a13b68cacc9726f3f
Joshua Crewe <me+git@joshuacrewe.co.uk>
2018-04-01 09:14:56 +0100

    Finish up responsive image support in posts

--- content/post/images.md
+++
@@ -1,7 +0,0 @@
----
-title: "Images"
-date: 2018-03-31T18:21:20+01:00
-draft: false
----
-
-{{< img class="" src="hello-world/test" type="jpg" alt="this is the alt">}}
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,8 @@
-// 1. Take images from images/* , create sizes and move them to static/uploads/images
-// 2. Do some image compressions and optimisations on images in static/uploads/images/**/*
-// 3. Make a shortcode which will translate images with src into a srcset with lazy loading
-// 4. Add lazy loading script so that images not quite in view are not loaded yet.
-
-
 import gulp from 'gulp';
 import responsive from 'gulp-responsive';
 
 gulp.task('images-resize', function() {
-    return gulp.src('./images/**/*.{png,jpg}')
+    return gulp.src('./content/images/**/*.{png,jpg}')
     .pipe(responsive({
         '**/*.jpg': [{
             width: 800,
@@ -23,7 +17,7 @@ gulp.task('images-resize', function() {
     }, {
         silent: true,
     }))
-    .pipe(gulp.dest('./static/uploads/images')
+    .pipe(gulp.dest('./static/content/images')
 )});
 
 gulp.task('default', ['images-resize']);
